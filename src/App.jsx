f249655e96698e0b70e332e4351e54c64b52a4fe
Baptiste Desharbes <desharbesbaptiste@hotmail.fr>
2024-11-22 12:43:57 +0100

    axios have been instal

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
       <h1>Our project</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
