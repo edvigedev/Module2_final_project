dd717e71c0d7f7117a264f11275a068a5e5e301f
Edvige Di Salvo <edvigedis@google.com>
2024-11-22 12:45:19 +0100

    delete first backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
+  useEffect(() => {
+    fetch("http://localhost:5005/projects")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   return (
     <>
       <h1>Our project</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
